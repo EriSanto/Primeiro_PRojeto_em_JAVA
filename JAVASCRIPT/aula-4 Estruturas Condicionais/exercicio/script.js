1393c324f2a97b62bfd9d1cde7e345ca8a4b8bd2
EriSanto <eriky.costa.santos@gmail.com>
2022-10-11 21:41:51 -0300

    aula4 JS

---
+++ JAVASCRIPT/aula-4 Estruturas Condicionais/exercicio/script.js
@@ -0,0 +1,43 @@
+
+//Exercício1
+var preço1 = 2.99;
+var preço2 = 4.41;
+var preço3 = 5.99;
+
+console.log
+    (`${preço3} ${preço2} Menor preço: ${preço1}`);
+
+//Exercício2
+
+
+var numero1 = 10
+var numero2 = 20
+var numero3 = 30
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var primeiro = 10;
+var segundo = 20;
+var terceiro = 30;
+ 
+console.log(`${primeiro} ${segundo} ${terceiro}`)*/
+
+//Exercício3
+const multiplo = 4;
+var resultado = 4 * 400
+console.log("Ano bissexto " + resultado)
+
+
+
+
+
