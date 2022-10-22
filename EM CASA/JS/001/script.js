4cb40faa50ee8a2641201f8e689988c58025352c
Eriky Costa Santos eriky <eriky.costa.santos@gmai.com>
2022-10-22 13:03:27 -0300

    Exercícios das aulas 7,8,9 e 10 de JS

---
+++ EM CASA/JS/001/script.js
@@ -0,0 +1,3 @@
+window.alert('Minha primeira mensagem'); //chamar alerta
+window.confirm('opa meu guerreiro'); // botão ok e cancelar
+window.prompt('Qual é seu nome?'); // perguntar seu nome
---
+++ JAVASCRIPT/aula-7 Array de Objetos/exercicios/script1.js
@@ -0,0 +1,62 @@
+let alunos = [
+    {
+        nome: 'Linna',
+        idade: 11,
+        notas: [6, 3, 15, 9],
+        sala: '3A',
+        escola: 'EM Cabral junior',
+        endereco: {
+            rua: 'Rua dos mosqueteiros',
+            numero: 14,
+            cidade: 'São Paulo',
+            estado: 'SP',
+        },
+        escolaridade: '2 grau do Ensino Médio',
+        responsável: 'Matheus - Pai',
+    },
+    {
+        nome: 'Thomas',
+        idade: 15,
+        notas: [5, 8, 4, 7],
+        sala: '33',
+        escola: 'Capitão Amaral',
+        endereco: {
+            rua: 'Rua dos patos',
+            numero: 28,
+            cidade: 'São Paulo',
+            estado: 'SP',
+        },
+        escolaridade: '1 grau do Ensino Médio',
+        responsável: 'Maria - Mãe',
+    },
+    {
+        nome: 'Gabi',
+        idade: 17,
+        notas: [5, 8, 8, 7],
+        sala: '54',
+        escola: 'Capitão Pedroso',
+        endereco: {
+            rua: 'Rua alameda salete',
+            numero: 89,
+            cidade: 'São Paulo',
+            estado: 'SP',
+        },
+        escolaridade: '1 grau do Ensino Médio',
+        responsável: 'Marcos - Pai',
+    },
+];
+
+let soma = 0;
+let media = 0;
+let aprov = 0;
+
+for (let x = 0; x < alunos.length; x++) {
+    soma = 0;
+    for (let i = 0; i < alunos[x].notas.length; i++) soma += alunos[x].notas[i];
+
+    media = soma / alunos[x].notas.length;
+    console.log(`media ${media}`);
+
+    aprov = media >= 7 ? 'aprovado' : 'reprovado';
+    console.log(` Aluno ${aprov}`);
+}
