ee8fa7d1b62d207050bfaf3d5b13708819020a38
EriSanto <eriky.costa.santos@gmail.com>
2022-11-08 21:15:52 -0300

    aula03 REact

---
+++ REACT/03_ComponentesFuncionais/tema_03_conta_cliques/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Exemplo from './Componentes/Exemplo';
+import Header from './Componentes/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Exemplo />
+      
+    </div>
+  );
+}
+
+export default App;
