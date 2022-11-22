c84c57224453dec0aa1a12bfee1a972bebe78c65
EriSanto <eriky.costa.santos@gmail.com>
2022-11-22 18:13:31 -0300

    JAVA

---
+++ JAVA/Primeiro projeto JAVA/src/calcular.java
@@ -0,0 +1,12 @@
+public class calcular {
+    // primeiro passo criar metodo main
+    public static void main(String[] args){
+        // Vamos somar a idade de três alunos
+        int idade1 = 25; // usar int para declarar um numero inteiro
+        int idade2 = 15;
+        int somaIdade = idade1 + idade2; // double e float para usar em numeros quebrados
+        System.out.println("A soma da idade dos alunos é: " + somaIdade);
+
+
+    }
+}
---
+++ JAVA/Primeiro projeto JAVA/src/pipoca.java
@@ -0,0 +1,11 @@
+public class pipoca {
+    // mostrar no console a frase Hello World
+    public static void  main(String[] args)
+    {
+       System.out.println("Hello World!"); //println para quebra de linha
+        String texto = "Teste";
+        System.out.println(texto);
+       System.out.print(45);
+       //console.log("Hello World!")
+    }
+}
