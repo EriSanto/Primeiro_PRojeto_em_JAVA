94391ba88e8eea812df634dee48066db567fecdf
EriSanto <eriky.costa.santos@gmail.com>
2022-11-22 20:58:01 -0300

    Aula 02 JAVA

---
+++ JAVA/Java 03 - Tipos de Dados e Operadores/Exercicio1/src/Exercicio1.java
@@ -0,0 +1,11 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        int nota1 = 10;
+        int nota2 = 6;
+        int nota3 = 7;
+        int nota4 = 6;
+
+        System.out.println("Média: " + (nota1 + nota2 + nota3 + nota4) /4);
+
+    }
+}
---
+++ JAVA/Java 03 - Tipos de Dados e Operadores/Exercicio1/src/Exercicio2.java
@@ -0,0 +1,7 @@
+public class Exercicio2 {
+        public static void main(String[] args) {
+            int base = 23;
+            int altura = 40;
+            System.out.print("A área do triângulo é:" + ((base * altura)/2));
+        }
+  }
